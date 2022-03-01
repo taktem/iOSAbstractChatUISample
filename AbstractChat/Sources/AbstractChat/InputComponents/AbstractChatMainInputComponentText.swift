6d123483526f89138070bc959151dfbd8b006e6b
taktem <nishimura@taktem.com>
2022-03-02 00:21:16 +0900

    :gem: メッセージ入力基本コンポーネント追加

---
+++ AbstractChat/Sources/AbstractChat/InputComponents/AbstractChatMainInputComponentText.swift
@@ -0,0 +1,53 @@
+//
+//  AbstractChatInputCompo.swift
+//  
+//
+//  Created by Taku Nishimura on 2022/03/01.
+//
+
+import UIKit
+import UIUtility
+
+public final class AbstractChatMainInputComponentText: UIView, AbstractChatMainInputComponent {
+
+    @IBOutlet private var textView: UITextView!
+    @IBOutlet private var textViewHeightConstraint: NSLayoutConstraint!
+
+    private let didSubmitText: ((String) -> Void)
+
+    public init(
+        didSubmitText: @escaping ((String) -> Void)
+    ) {
+        self.didSubmitText = didSubmitText
+        super.init(frame: .zero)
+        let nib = UINib(nibName: "AbstractChatMainInputComponentText", bundle: Bundle.module)
+        let view = nib.instantiate(withOwner: self, options: nil).first as! UIView
+        addSubview(view)
+        view.snap(to: self)
+        textView.delegate = self
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    @IBAction private func didTapSendButton() {
+        guard let text = textView.text else { return }
+        textView.text = nil
+        adjustTextViewHeight()
+        didSubmitText(text)
+
+        endEditing(true)
+    }
+
+    private func adjustTextViewHeight() {
+        let height = textView.sizeThatFits(CGSize(width: textView.frame.size.width, height: CGFloat.greatestFiniteMagnitude)).height
+        textViewHeightConstraint.constant = height
+    }
+}
+
+extension AbstractChatMainInputComponentText: UITextViewDelegate {
+    public func textViewDidChange(_ textView: UITextView) {
+        adjustTextViewHeight()
+    }
+}

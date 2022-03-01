10ae951f3db5922f72f800feb932e5a3c31b74b4
taktem <nishimura@taktem.com>
2022-03-02 00:22:41 +0900

    :fire: Util化したextension削除

--- iOSAbstractChatUISample/ChatContainerViewController.swift
+++ iOSAbstractChatUISample/ChatContainerViewController.swift
@@ -80,14 +80,3 @@ final class ChatContainerViewController: UIViewController {
         chatViewController.configure(messages: messeges)
     }
 }
-
-private extension UIView {
-    func snap(to view: UIView, edgeInsets: UIEdgeInsets = .zero) {
-        self.translatesAutoresizingMaskIntoConstraints = false
-        self.topAnchor.constraint(equalTo: view.topAnchor, constant: edgeInsets.top).isActive = true
-        self.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -edgeInsets.bottom).isActive = true
-
-        self.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: edgeInsets.left).isActive = true
-        self.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -edgeInsets.right).isActive = true
-    }
-}
